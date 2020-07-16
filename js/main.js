db58c39e2005a33c866ab693d292e42b45fc0579
dhruvanshmoyal01 <dhruvanshm.official@gmail.com>
2020-07-16 17:27:22 +0530

    adding projects page

---
+++ js/main.js
@@ -0,0 +1,19 @@
+$(document).ready(function(){
+
+	let $btns = $('.project-display .button-group button'); //not working
+
+	$btns.click(function(e){
+			
+		$('.project-display .button-group button').removeClass("active");
+		e.target.classList.add("active");
+
+		let selector = $(e.target).attr("data-filter");
+		$('.project-display .grid').isotope({
+			filter:selector
+		});
+		return false;
+	})
+
+	$('project-area .button-group #btn1').trigger("click");
+
+});
